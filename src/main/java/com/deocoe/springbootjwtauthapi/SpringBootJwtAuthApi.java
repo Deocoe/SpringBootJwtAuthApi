87b9c57e5c6e0ec1e98b001772a66c29c7906a13
Daniel Coelho <deocoe@gmail.com>
2025-10-01 19:36:37 -0300

    build: Initial project structure with Spring Initializr

---
+++ src/main/java/com/deocoe/springbootjwtauthapi/SpringBootJwtAuthApi.java
@@ -0,0 +1,13 @@
+package com.deocoe.springbootjwtauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtAuthApi.class, args);
+    }
+
+}
