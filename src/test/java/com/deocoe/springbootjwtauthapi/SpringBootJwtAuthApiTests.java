c4aa90616980f54d265ff7a93bf1df9ddb46c475
Daniel Coelho <deocoe@gmail.com>
2025-10-01 19:46:33 -0300

    build: Add database configuration, tests and gitignore

---
+++ src/test/java/com/deocoe/springbootjwtauthapi/SpringBootJwtAuthApiTests.java
@@ -0,0 +1,13 @@
+package com.deocoe.springbootjwtauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthApiTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
